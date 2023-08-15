f5abd7fd3e668b11a9eac87c779f5c6160aa2a4f
xhyu <xhyu@qeelyn.com>
2023-08-15 16:13:12 +0800

    examples增加真实插件

--- examples/with-urql/ice.config.mts
+++ examples/with-urql/ice.config.mts
@@ -1,12 +1,31 @@
 import { defineConfig } from '@ice/app';
 import store from '@ice/plugin-store';
 import urqlPlugin from '@knockout-js/ice-urql';
-import request  from "@ice/plugin-request";
+import request from "@ice/plugin-request";
+import jsxPlus from '@ice/plugin-jsx-plus';
+import auth from '@ice/plugin-auth';
+import antd from '@ice/plugin-antd';
+
+const minify = process.env.NODE_ENV === 'production' ? 'swc' : 'swc';
 
 export default defineConfig(() => ({
+  ssg: false,
+  ssr: false,
+  minify,
+  codeSplitting: 'page',
+  routes: {
+    ignoreFiles: [
+      '**/components/**',   // 添加此配置忽略components被解析成路由组件
+    ],
+  },
   plugins: [
-    store(),
-    request(),
     urqlPlugin(),
+    request(),
+    store(),
+    jsxPlus(),
+    antd({
+      importStyle: false,
+    }),
+    auth(),
   ],
 }));
--- examples/with-urql/src/app.tsx
+++ examples/with-urql/src/app.tsx
@@ -1,8 +1,10 @@
 import { defineDataLoader } from '@ice/runtime';
+import { defineAuthConfig } from '@ice/plugin-auth/esm/types';
 import { defineStoreConfig } from '@ice/plugin-store/esm/types';
 import { defineRequestConfig } from '@ice/plugin-request/esm/types';
 import { defineUrqlConfig, requestInterceptor } from "@knockout-js/ice-urql/esm/types";
 import store from './store';
+import { userPermissions } from '@knockout-js/api';
 
 export default {
   app: {
@@ -11,7 +13,6 @@ export default {
 };
 
 export const dataLoader = defineDataLoader(async () => {
-  console.log('defineDataLoader')
   return {
     user: {
       token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE2OTEzOTcwMDB9.OxViESAOpW8J1pqMVCE0ObOg7nu2-um9SCXn7gR2bdY',
@@ -22,9 +23,60 @@ export const dataLoader = defineDataLoader(async () => {
   }
 });
 
+export const urqlConfig = defineUrqlConfig([
+  {
+    instanceName: 'default',
+    url: 'https://trygql.formidable.dev/graphql/basic-pokedex',
+    // exchangeOpt: {
+    //   authOpts: {
+    //     store: {
+    //       getState: () => {
+    //         const { token, tenantId, refreshToken } = store.getModelState('user')
+    //         return {
+    //           token, tenantId, refreshToken
+    //         }
+    //       },
+    //       setStateToken: (newToken) => {
+    //         store.dispatch.user.updateToken(newToken)
+    //       }
+    //     },
+    //     login: '/login',
+    //     refreshApi: "/api-auth/login/refresh-token"
+    //   }
+    // }
+  },
+  {
+    instanceName: 'instance2',
+    url: 'https://trygql.formidable.dev/graphql/basic-pokedex',
+  },
+  {
+    instanceName: 'ucenter',
+    url: 'http://127.0.0.1:3001/mock-api-adminx/graphql/query',
+  },
+])
+
+// 权限
+export const authConfig = defineAuthConfig(async (appData) => {
+  const { user } = appData,
+    initialAuth = {};
+  // 判断路由权限
+  if (user.token) {
+    const result = await userPermissions("test");
+    if (result) {
+      result.forEach(item => {
+        if (item) {
+          initialAuth[item.name] = true;
+        }
+      });
+    }
+  }
+  return {
+    initialAuth,
+  };
+});
+
 // store数据项
 export const storeConfig = defineStoreConfig(async (appData) => {
-  console.log('defineStoreConfig')
   const { user } = appData;
   return {
     initialStates: {
@@ -33,42 +85,7 @@ export const storeConfig = defineStoreConfig(async (appData) => {
   };
 });
 
-export const urqlConfig = defineUrqlConfig(async () => {
-  console.log('defineUrqlConfig')
-
-  return [
-    {
-      instanceName: 'default',
-      url: 'https://trygql.formidable.dev/graphql/basic-pokedex',
-      // exchangeOpt: {
-      //   authOpts: {
-      //     store: {
-      //       getState: () => {
-      //         const { token, tenantId, refreshToken } = store.getModelState('user')
-      //         return {
-      //           token, tenantId, refreshToken
-      //         }
-      //       },
-      //       setStateToken: (newToken) => {
-      //         store.dispatch.user.updateToken(newToken)
-      //       }
-      //     },
-      //     login: '/login',
-      //     refreshApi: "/api-auth/login/refresh-token"
-      //   }
-      // }
-    },
-    {
-      instanceName: 'instance2',
-      url: 'https://trygql.formidable.dev/graphql/basic-pokedex',
-    },
-    {
-      instanceName: 'ucenter',
-      url: 'http://127.0.0.1:3001/mock-api-adminx/graphql/query',
-    },
-  ]
-})
-
+// 请求配置
 export const requestConfig = defineRequestConfig(() => ({
   baseURL: '/',
   interceptors: requestInterceptor({
